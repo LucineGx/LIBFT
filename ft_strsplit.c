28b79d2ed44426efad415c4d04485d3eef6b893d
lemair_l <cyril.lemaire@epitech.eu>
2016-12-07 12:37:47 +0100

    new strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,72 +1,59 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strsplit.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lgaveria <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/16 19:22:09 by lgaveria          #+#    #+#             */
-/*   Updated: 2016/12/01 16:36:34 by lgaveria         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stdio.h>
 #include "libft.h"
 
 static int		ctwords(char const *s, char delim)
 {
-	int	i;
-	int	count;
+  int	i;
+  int	count;
 
-	i = 0;
-	count = 0;
-	while (s[i] == delim)
-		i += 1;
-	while (s[i])
+  i = 0;
+  count = 0;
+  while (s[i])
+    {
+      if (s[i] != delim)
 	{
-		count += 1;
-		while (s[i] != delim)
-			i += 1;
-		while (s[i] == delim)
-			i += 1;
+	  if(s[i + 1] == delim || !s[i + 1])
+	    count += 1;
 	}
-	return (count);
+      i += 1;
+    }
+  return (count);
 }
 
 static int		ctletters(char const *s, char delim)
 {
-	int	count;
+  int	count;
 
-	count = 0;
-	while (s[count] != delim)
-		count += 1;
-	return (count);
+  count = 0;
+  while (s[count] != delim)
+    count += 1;
+  return (count);
 }
 
-char			**ft_strsplit(char const *s, char c)
+char	**ft_strsplit(char const *s, char c)
 {
-	char	**ret;
-	int		i;
-	int		j;
-	int		k;
+  char	**ret;
+  int	i;
+  int	j;
+  int	k;
 
-	if ((ret = malloc(sizeof(char *) * ctwords(s, c))) == 0)
-		return (0);
-	k = -1;
-	i = 0;
-	while (s[++k])
+  if ((ret = malloc(sizeof(char *) * (ctwords(s, c) + 1))) == 0)
+    return (0);
+  k = -1;
+  i = 0;
+  j = 0;
+  while (s[++k])
+    {
+      i += (j != 0 && s[k] == c) ? 1 : 0;
+      j = (s[k] == c) ? 0 : j;
+      if (s[k] != c)
 	{
-		if (s[k] != c)
-		{
-			j = 0;
-			if ((ret[i] = malloc(sizeof(char) * ctletters(&s[k], c)) + 1) == 0)
-				return (0);
-			k -= 1;
-			while (s[++k] != c && s[k])
-				ret[i][j++] = s[k];
-			ret[i][j] = '\0';
-			i += 1;
-		}
-	}
-	return (ret);
+	  if (j == 0)
+	    if ((ret[i] = malloc(sizeof(char) * (ctletters(&s[k], c) + 1))) == 0)
+		return (0);
+	  ret[i][j++] = s[k];
+	  ret[i][j] = '\0';
+	 }
+    }
+      ret[++i] = (j != 0) ? '\0' : ret[++i];
+  return (ret);
 }
